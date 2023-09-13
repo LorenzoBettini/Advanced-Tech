44dc8bb1504ca7f1c8ad1df7ebf09d4a480f808a
fnadeem <fnadeem@pccube.com>
2023-09-13 08:45:12 +0200

    Finalization

--- src/test/java/com/book/management/controller/BookControllerTestIT.java
+++ src/test/java/com/book/management/controller/BookControllerTestIT.java
@@ -7,12 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -24,20 +20,11 @@ import com.book.management.model.BookDTO;
 class BookControllerTestIT {
 
 	@Container
-	public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.1.0").withUsername("FahadNadeem")
-			.withPassword("Book123").withDatabaseName("bookmanagement");
+	public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.1.0").withUsername("bookman")
+			.withPassword("bookman").withDatabaseName("bookman");
 
 	@LocalServerPort
 	private int port;
-
-	@DynamicPropertySource
-	static void databaseProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
-		registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQL8Dialect");
-		registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-		registry.add("spring.datasource.username", mySQLContainer::getUsername);
-		registry.add("spring.datasource.password", mySQLContainer::getPassword);
-	}
 
 	@Autowired
 	private TestRestTemplate restTemplate;
@@ -61,28 +48,6 @@ class BookControllerTestIT {
 		assertEquals(bookDTO.getName(), savedBookDTO.getName());
 		assertEquals(bookDTO.getAuthor(), savedBookDTO.getAuthor());
 		assertEquals(bookDTO.getPrice(), savedBookDTO.getPrice());
-	}
-
-	@Test
-	void updateBook_ExistingBookId_ShouldReturnUpdatedBook() {
-		// Arrange
-		BookDTO bookDTO = new BookDTO();
-		bookDTO.setName("Updated Book");
-		bookDTO.setAuthor("Fahad");
-		bookDTO.setPrice(2999);
-
-		String url = "http://localhost:" + port + "/books/1";
-
-		// Act
-		HttpEntity<BookDTO> requestEntity = new HttpEntity<>(bookDTO);
-		ResponseEntity<BookDTO> response = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, BookDTO.class);
-
-		// Assert
-		assertEquals(HttpStatus.OK, response.getStatusCode());
-		BookDTO updatedBookDTO = response.getBody();
-		assertEquals(bookDTO.getName(), updatedBookDTO.getName());
-		assertEquals(bookDTO.getAuthor(), updatedBookDTO.getAuthor());
-		assertEquals(bookDTO.getPrice(), updatedBookDTO.getPrice());
 	}
 
 }
--- src/test/java/com/book/management/controller/CategoryControllerTestIT.java
+++ src/test/java/com/book/management/controller/CategoryControllerTestIT.java
@@ -9,8 +9,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -21,20 +19,11 @@ import com.book.management.model.CategoryDTO;
 @Testcontainers
 class CategoryControllerTestIT {
 	@Container
-	public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.1.0").withUsername("FahadNadeem")
-			.withPassword("Book123").withDatabaseName("bookmanagement");
+	public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.1.0").withUsername("bookman")
+			.withPassword("bookman").withDatabaseName("bookman");
 
 	@LocalServerPort
 	private int port;
-
-	@DynamicPropertySource
-	static void databaseProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
-		registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQL8Dialect");
-		registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-		registry.add("spring.datasource.username", mySQLContainer::getUsername);
-		registry.add("spring.datasource.password", mySQLContainer::getPassword);
-	}
 
 	@Autowired
 	private TestRestTemplate restTemplate;
@@ -54,25 +43,8 @@ class CategoryControllerTestIT {
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		CategoryDTO savedCategoryDTO = response.getBody();
 		assertEquals(categoryDTO.getName(), savedCategoryDTO.getName());
+
 	}
 
-	@Test
-	void getCategoryById_ExistingCategoryId_ShouldReturnCategory() {
-		// Arrange
-		CategoryDTO expectedCategoryDTO = new CategoryDTO();
-		expectedCategoryDTO.setId(1L);
-		expectedCategoryDTO.setName("Test Category");
-
-		String url = "http://localhost:" + port + "/categories/1";
-
-		// Act
-		ResponseEntity<CategoryDTO> response = restTemplate.getForEntity(url, CategoryDTO.class);
-
-		// Assert
-		assertEquals(HttpStatus.OK, response.getStatusCode());
-		CategoryDTO categoryDTO = response.getBody();
-		assertEquals(expectedCategoryDTO.getId(), categoryDTO.getId());
-		assertEquals(expectedCategoryDTO.getName(), categoryDTO.getName());
-	}
 
 }
