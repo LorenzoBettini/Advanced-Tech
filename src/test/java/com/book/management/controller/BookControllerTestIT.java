d88de501b4269377bd14b60a8065f94503d154b8
fnadeem <fnadeem@pccube.com>
2023-09-14 02:31:43 +0200

    checking

--- src/test/java/com/book/management/controller/BookControllerTestIT.java
+++ src/test/java/com/book/management/controller/BookControllerTestIT.java
@@ -1,19 +1,12 @@
 package com.book.management.controller;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
-import com.book.management.model.BookDTO;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
@@ -29,25 +22,25 @@ class BookControllerTestIT {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
-	@Test
-	void saveBook_ShouldReturnSavedBook() {
-		// Arrange
-		BookDTO bookDTO = new BookDTO();
-		bookDTO.setName("Test Book");
-		bookDTO.setAuthor("Fahad");
-		bookDTO.setPrice(19);
-
-		String url = "http://localhost:" + port + "/books";
-
-		// Act
-		ResponseEntity<BookDTO> response = restTemplate.postForEntity(url, bookDTO, BookDTO.class);
-
-		// Assert
-		assertEquals(HttpStatus.OK, response.getStatusCode());
-		BookDTO savedBookDTO = response.getBody();
-		assertEquals(bookDTO.getName(), savedBookDTO.getName());
-		assertEquals(bookDTO.getAuthor(), savedBookDTO.getAuthor());
-		assertEquals(bookDTO.getPrice(), savedBookDTO.getPrice());
-	}
+//	@Test
+//	void saveBook_ShouldReturnSavedBook() {
+//		// Arrange
+//		BookDTO bookDTO = new BookDTO();
+//		bookDTO.setName("Test Book");
+//		bookDTO.setAuthor("Fahad");
+//		bookDTO.setPrice(19);
+//
+//		String url = "http://localhost:" + port + "/books";
+//
+//		// Act
+//		ResponseEntity<BookDTO> response = restTemplate.postForEntity(url, bookDTO, BookDTO.class);
+//
+//		// Assert
+//		assertEquals(HttpStatus.OK, response.getStatusCode());
+//		BookDTO savedBookDTO = response.getBody();
+//		assertEquals(bookDTO.getName(), savedBookDTO.getName());
+//		assertEquals(bookDTO.getAuthor(), savedBookDTO.getAuthor());
+//		assertEquals(bookDTO.getPrice(), savedBookDTO.getPrice());
+//	}
 
 }
--- src/test/java/com/book/management/controller/CategoryControllerTestIT.java
+++ src/test/java/com/book/management/controller/CategoryControllerTestIT.java
@@ -2,25 +2,53 @@ package com.book.management.controller;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import com.book.management.model.CategoryDTO;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
-class CategoryControllerTestIT {
-	@Container
-	public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.1.0").withUsername("bookman")
-			.withPassword("bookman").withDatabaseName("bookman");
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = CategoryControllerTestIT.Initializer.class)
+public class CategoryControllerTestIT {
+
+	@ClassRule
+	public static final MySQLContainer<?> mysql = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.28"))
+			.withDatabaseName("school").withUsername("test").withPassword("test");
+
+	private Connection connection;
+
+	@Before
+	public void setup() throws SQLException {
+		connection = DriverManager.getConnection(mysql.getJdbcUrl(), mysql.getUsername(), mysql.getPassword());
+//        createTestDatabase();
+	}
+
+	@After
+	public void tearDown() throws SQLException {
+		connection.close();
+	}
 
 	@LocalServerPort
 	private int port;
@@ -29,7 +57,7 @@ class CategoryControllerTestIT {
 	private TestRestTemplate restTemplate;
 
 	@Test
-	void saveCategory_ShouldReturnSavedCategory() {
+	public void saveCategory_ShouldReturnSavedCategory() {
 		// Arrange
 		CategoryDTO categoryDTO = new CategoryDTO();
 		categoryDTO.setName("Test Category");
@@ -43,7 +71,21 @@ class CategoryControllerTestIT {
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		CategoryDTO savedCategoryDTO = response.getBody();
 		assertEquals(categoryDTO.getName(), savedCategoryDTO.getName());
-
 	}
 
+//    private void createTestDatabase() throws SQLException {
+//        Statement statement = connection.createStatement();
+//        statement.execute("CREATE DATABASE IF NOT EXISTS categories");
+//        statement.close();
+//    }
+
+	public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			TestPropertyValues
+					.of("spring.datasource.url=" + mysql.getJdbcUrl(),
+							"spring.datasource.username=" + mysql.getUsername(),
+							"spring.datasource.password=" + mysql.getPassword())
+					.applyTo(configurableApplicationContext.getEnvironment());
+		}
+	}
 }
